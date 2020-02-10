0fa8df1f02dc680cb468b82be22a517c985a3469
pseudo <Mitchell.Wright877@gmail.com>
2020-02-10 07:05:06 +0000

    Modified to handle configurable key-size

--- LaMesaServer/src/com/lamesa/util/TextFormat.java
+++ LaMesaServer/src/com/lamesa/util/TextFormat.java
@@ -2,15 +2,22 @@ package com.lamesa.util;
 
 import java.util.Date;
 
-public class TextFormat {
+import com.lamesa.net.ClientHandler;
 
+public class TextFormat {
+	
+	/**
+	 * Format a key into 32 2-digit hex segments
+	 * @param key
+	 * @return string representation
+	 */
 	public static String formatKey(byte[] key) {
 		StringBuilder sb = new StringBuilder();
 		
-		for(int i = 0; i < 31; i++) {
+		for(int i = 0; i < ClientHandler.KEY_SIZE - 1; i++) {
 			sb.append(String.format("%02X:", key[i]));
 		}
-		sb.append(String.format("%02X", key[31]));
+		sb.append(String.format("%02X", key[ClientHandler.KEY_SIZE - 1]));
 		
 		return sb.toString();
 	}
